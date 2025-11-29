157cbc13cb1e848f8888ef1b8169b79868ea66e4
Wiam Abdellaoui <wabdella@e1r12p3.1337.ma>
2025-11-29 23:02:49 +0100

    f

--- get_next_line.c
+++ get_next_line.c
@@ -1,4 +1,5 @@
 # include "get_next_line.h"
+// if char contains back to line it gives its index otherwise it returns -1
 size_t not_back_to_line(char * buffer){
     size_t i=0;
     while(buffer[i]){
@@ -6,7 +7,7 @@ size_t not_back_to_line(char * buffer){
             return i;
         i++;
     }
-    return 0;
+    return -1;
 }
 void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
@@ -26,13 +27,65 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	}
 	return (d);
 }
+void * ft_join (char * line,size_t size_line, char *buffer,size_t read_size)
+{
+    char * new_line;   
+    new_line = malloc (sizeof(char)*(read_size+size_line+1)) ;
+    size_t i=0;
+    while(i< size_line){
+        new_line[i]=line[i];
+        i++;
+    }
+    while(i< read_size + size_line){
+        new_line[i]=buffer[i-size_line];
+        i++;
+    }
+    new_line[i]='\0';
+    free(line);
+    return new_line;
+
+}
+char *get_next_line(int fd){
+    if( fd == NULL || BUFFER_SIZE <= 0)
+        return NULL;
+    static char * line;
+    size_t size_line=0;
+    size_t read_size;
+    char *buffer;
+    buffer = malloc(sizeof(char)*(BUFFER_SIZE+1));
+    if(!buffer){
+            return NULL;
+        }
+    while(!not_back_to_line(buffer)){
+        read_size = read(fd, buffer, BUFFER_SIZE);
+        size_line =+read_size;
+        if (size_line <= 0)
+            return NULL;
+        line = ft_join(line,size_line, buffer,read_size);
+        
+
+    }
+    
+
+}
+
+
+
+
+
+
+
+
+
 
 char *get_next_line(int fd){
     char buffer[BUFFER_SIZE + 1];
     static char rest[BUFFER_SIZE + 1];
+    fill_with_0(rest,BUFFER_SIZE+1);
     char *result;
     char *tmp;
     char *tmp2;
+    size_t bytes;
     size_t i =0;
     size_t size=0;
     tmp2 = malloc(sizeof(char)*(BUFFER_SIZE+1));
